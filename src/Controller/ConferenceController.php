2f119016d7e3bf1e2829e68f5f806abf7c8e6c4b
MasonVyGonyte <vinnikov.2018@mail.ru>
2025-06-07 18:24:04 +0300

    Реализация смены документов 2

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -17,41 +17,70 @@ class ConferenceController extends AbstractController
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
             'controller_path' => __FILE__,
-            'template_path' => '/templates/conference/index.html.twig' // Путь к шаблону (передаём как строку)
+            'template_path' => '/templates/conference/index.html.twig' 
       
         ]);
     }
     
     #[Route('/submit', name: 'handle_form', methods: ['POST'])]
-    public function handleForm(Request $request): Response
-    {
-        $name = $request->request->get('name');
-$secondName = $request->request->get('secondName');
-$thirdName = $request->request->get('thirdName');
-$age = $request->request->get('age');
+public function handleForm(Request $request): Response
+{
+    $documentType = $request->request->get('document_type');
+    // Получаем данные из формы
+    $name = $request->request->get('name');
+    $secondName = $request->request->get('secondName');
+    $thirdName = $request->request->get('thirdName');
+    $age = $request->request->get('age');
+    $numberPassport = $request->request->get('numberPassport');
+    $serialPassport = $request->request->get('serialPassport');
+    $passportIssuedBy = $request->request->get('passportIssuedBy');
+    $passportDate = $request->request->get('passportDate');
+    $address = $request->request->get('address');
 
-$numberPassport = $request->request->get('numberPassport');
-$serialPassport = $request->request->get('serialPassport');
-$passportIssuedBy = $request->request->get('passportIssuedBy');
-$passportDate = $request->request->get('passportDate');
-$address = $request->request->get('address');
+    // Второй набор данных
+    $name2 = $request->request->get('name2');
+    $secondName2 = $request->request->get('secondName2');
+    $thirdName2 = $request->request->get('thirdName2');
+    $age2 = $request->request->get('age2');
+    $numberPassport2 = $request->request->get('numberPassport2');
+    $serialPassport2 = $request->request->get('serialPassport2');
+    $passportIssuedBy2 = $request->request->get('passportIssuedBy2');
+    $passportDate2 = $request->request->get('passportDate2');
+    $address2 = $request->request->get('address2');
 
+    // Получаем сессию
+    $session = $request->getSession();
 
-        $session = $request->getSession();
-        $session->set('name', $name);
-        $session->set('secondName', $secondName);
-        $session->set('thirdName', $thirdName);
-        $session->set('age', $age);
-        $session->set('numberPassport', $numberPassport);
-        $session->set('serialPassport', $serialPassport);
-        $session->set('passportIssuedBy', $passportIssuedBy);
-        $session->set('passportDate', $passportDate);
-        $session->set('address', $address);
-        
+    // Сохраняем данные из первого набора
+    $session->set('name', $name);
+    $session->set('secondName', $secondName);
+    $session->set('thirdName', $thirdName);
+    $session->set('age', $age);
+    $session->set('numberPassport', $numberPassport);
+    $session->set('serialPassport', $serialPassport);
+    $session->set('passportIssuedBy', $passportIssuedBy);
+    $session->set('passportDate', $passportDate);
+    $session->set('address', $address);
 
-        return $this->redirectToRoute('generate_word');      
-        
-    }
+    // Сохраняем данные из второго набора
+    $session->set('name2', $name2);
+    $session->set('secondName2', $secondName2);
+    $session->set('thirdName2', $thirdName2);
+    $session->set('age2', $age2);
+    $session->set('numberPassport2', $numberPassport2);
+    $session->set('serialPassport2', $serialPassport2);
+    $session->set('passportIssuedBy2', $passportIssuedBy2);
+    $session->set('passportDate2', $passportDate2);
+    $session->set('address2', $address2);
+
+    
+
+    // Сохраняем document_type в сессии
+    $session->set('document_type', $documentType);
+
+    return $this->redirectToRoute('generate_word'); 
+}
+
     
     
     #[Route('/', name: 'homepage')]
--- src/Controller/WordFileController.php
+++ src/Controller/WordFileController.php
@@ -16,27 +16,49 @@ class WordFileController extends AbstractController
     #[Route('/download-word', name: 'generate_word')]
     public function generateWord(Request $request): Response
     {
-      
+        
+        
+
         $session = $request->getSession();
+
+        $documentType = $session->get('document_type');
+
         $name = $session->get('name');
         $secondName = $session->get('secondName');
         $thirdName = $session->get('thirdName');
         $age = $session->get('age');
         $numberPassport = $session->get('numberPassport');
-$serialPassport = $session->get('serialPassport');
-$passportIssuedBy = $session->get('passportIssuedBy');
-$passportDate = $session->get('passportDate');
-$address = $session->get('address');
-     
+        $serialPassport = $session->get('serialPassport');
+        $passportIssuedBy = $session->get('passportIssuedBy');
+        $passportDate = $session->get('passportDate');
+        $address = $session->get('address');
+
+        $name2 = $session->get('name2');
+        $secondName2 = $session->get('secondName2');
+        $thirdName2 = $session->get('thirdName2');
+        $age2 = $session->get('age2');
+        $numberPassport2 = $session->get('numberPassport2');
+        $serialPassport2 = $session->get('serialPassport2');
+        $passportIssuedBy2 = $session->get('passportIssuedBy2');
+        $passportDate2 = $session->get('passportDate2');
+        $address2 = $session->get('address2');
+
+        $templates = [
+            'doc1' => '/public/templates/d1.docx',
+            'doc2' => '/public/templates/d2.docx',
+            'doc3' => '/public/templates/d3.docx',
+            'doc4' => '/public/templates/d4.docx',
+        ];
        
-        $templatePath = $this->getParameter('kernel.project_dir') . '/public/templates/M1.docx';
+        
+        $templatePath = $this->getParameter('kernel.project_dir') . $templates[$documentType];
 
         //существует ли файл
         if (!file_exists($templatePath)) {
             return new Response("Ошибка: шаблон не найден.");
         }
-        if (!$name) {
-            return new Response("нет данных для скачиваня");
+        if ($documentType) {
+            return new Response($documentType);
         }
        
         $templateProcessor = new TemplateProcessor($templatePath);
@@ -50,6 +72,16 @@ $address = $session->get('address');
         $templateProcessor->setValue('{{passportIssuedBy}}', $passportIssuedBy);
         $templateProcessor->setValue('{{passportDate}}', $passportDate);
         $templateProcessor->setValue('{{address}}', $address);
+
+        $templateProcessor->setValue('{{name2}}', $name2);
+        $templateProcessor->setValue('{{secondName2}}', $secondName2);
+        $templateProcessor->setValue('{{thirdName2}}', $thirdName2);
+        $templateProcessor->setValue('{{age2}}', $age2);
+        $templateProcessor->setValue('{{numberPassport2}}', $numberPassport2);
+        $templateProcessor->setValue('{{serialPassport2}}', $serialPassport2);
+        $templateProcessor->setValue('{{passportIssuedBy2}}', $passportIssuedBy2);
+        $templateProcessor->setValue('{{passportDate2}}', $passportDate2);
+        $templateProcessor->setValue('{{address2}}', $address2);
 
 
      
