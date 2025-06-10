1d32dd5d8bfbe7bbea7bfcaf0bb442a7f5783d79
MasonVyGonyte <vinnikov.2018@mail.ru>
2025-06-11 01:12:10 +0300

    Смена документов3

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -36,6 +36,13 @@ public function handleForm(Request $request): Response
     $passportIssuedBy = $request->request->get('passportIssuedBy');
     $passportDate = $request->request->get('passportDate');
     $address = $request->request->get('address');
+    $phone = $request->request->get('phone');
+    $email = $request->request->get('email');
+
+
+
+// Сохраняем данные в сессии
+
 
     // Второй набор данных
     $name2 = $request->request->get('name2');
@@ -47,6 +54,36 @@ public function handleForm(Request $request): Response
     $passportIssuedBy2 = $request->request->get('passportIssuedBy2');
     $passportDate2 = $request->request->get('passportDate2');
     $address2 = $request->request->get('address2');
+    $phone2 = $request->request->get('phone2');
+    $email2 = $request->request->get('email2');
+
+
+$name3 = $request->request->get('name3');
+$secondName3 = $request->request->get('secondName3');
+$thirdName3 = $request->request->get('thirdName3');
+$numberPassport3 = $request->request->get('numberPassport3');
+$serialPassport3 = $request->request->get('serialPassport3');
+$passportIssuedBy3 = $request->request->get('passportIssuedBy3');
+$passportDate3 = $request->request->get('passportDate3');
+$address3 = $request->request->get('address3');
+$phone3 = $request->request->get('phone3');
+$email3 = $request->request->get('email3');
+
+
+
+    $name4 = $request->request->get('name4');
+$secondName4 = $request->request->get('secondName4');
+$thirdName4 = $request->request->get('thirdName4');
+$name5 = $request->request->get('name5');
+$secondName5 = $request->request->get('secondName5');
+$thirdName5 = $request->request->get('thirdName5');
+$bday = $request->request->get('bday');
+$blocation = $request->request->get('blocation');
+$serialSvid = $request->request->get('serialSvid');
+$numSvid = $request->request->get('numSvid');
+$svidIssuedBy = $request->request->get('svidIssuedBy');
+$svidDate = $request->request->get('svidDate');
+$address4 = $request->request->get('address4');
 
     // Получаем сессию
     $session = $request->getSession();
@@ -61,6 +98,8 @@ public function handleForm(Request $request): Response
     $session->set('passportIssuedBy', $passportIssuedBy);
     $session->set('passportDate', $passportDate);
     $session->set('address', $address);
+    $session->set('phone', $phone);
+    $session->set('email', $email);
 
     // Сохраняем данные из второго набора
     $session->set('name2', $name2);
@@ -72,8 +111,35 @@ public function handleForm(Request $request): Response
     $session->set('passportIssuedBy2', $passportIssuedBy2);
     $session->set('passportDate2', $passportDate2);
     $session->set('address2', $address2);
+    $session->set('phone2', $phone2);
+    $session->set('email2', $email2);
 
-    
+    // Сохраняем данные в сессии
+$session->set('name3', $name3);
+$session->set('secondName3', $secondName3);
+$session->set('thirdName3', $thirdName3);
+$session->set('numberPassport3', $numberPassport3);
+$session->set('serialPassport3', $serialPassport3);
+$session->set('passportIssuedBy3', $passportIssuedBy3);
+$session->set('passportDate3', $passportDate3);
+$session->set('address3', $address3);
+$session->set('phone3', $phone3);
+$session->set('email3', $email3);
+
+
+    $session->set('name4', $name4);
+$session->set('secondName4', $secondName4);
+$session->set('thirdName4', $thirdName4);
+$session->set('name5', $name5);
+$session->set('secondName5', $secondName5);
+$session->set('thirdName5', $thirdName5);
+$session->set('bday', $bday);
+$session->set('blocation', $blocation);
+$session->set('serialSvid', $serialSvid);
+$session->set('numSvid', $numSvid);
+$session->set('svidIssuedBy', $svidIssuedBy);
+$session->set('svidDate', $svidDate);
+$session->set('address4', $address4);
 
     // Сохраняем document_type в сессии
     $session->set('document_type', $documentType);
--- src/Controller/WordFileController.php
+++ src/Controller/WordFileController.php
@@ -32,6 +32,8 @@ class WordFileController extends AbstractController
         $passportIssuedBy = $session->get('passportIssuedBy');
         $passportDate = $session->get('passportDate');
         $address = $session->get('address');
+        $phone = $session->get('phone');
+        $email = $session->get('email');
 
         $name2 = $session->get('name2');
         $secondName2 = $session->get('secondName2');
@@ -42,12 +44,47 @@ class WordFileController extends AbstractController
         $passportIssuedBy2 = $session->get('passportIssuedBy2');
         $passportDate2 = $session->get('passportDate2');
         $address2 = $session->get('address2');
+        $phone2 = $session->get('phone2');
+        $email2 = $session->get('email2');
+
+        $name3 = $session->get('name3');
+$secondName3 = $session->get('secondName3');
+$thirdName3 = $session->get('thirdName3');
+$numberPassport3 = $session->get('numberPassport3');
+$serialPassport3 = $session->get('serialPassport3');
+$passportIssuedBy3 = $session->get('passportIssuedBy3');
+$passportDate3 = $session->get('passportDate3');
+$address3 = $session->get('address3');
+$phone3 = $session->get('phone3');
+$email3 = $session->get('email3');
+
+// Обновляем шаблон для doc3
+
+
+        $name4 = $session->get('name4');
+$secondName4 = $session->get('secondName4');
+$thirdName4 = $session->get('thirdName4');
+$name5 = $session->get('name5');
+$secondName5 = $session->get('secondName5');
+$thirdName5 = $session->get('thirdName5');
+$bday = $session->get('bday');
+$blocation = $session->get('blocation');
+$serialSvid = $session->get('serialSvid');
+$numSvid = $session->get('numSvid');
+$svidIssuedBy = $session->get('svidIssuedBy');
+$svidDate = $session->get('svidDate');
+$address4 = $session->get('address4');
+
+// Обновляем шаблон для doc3
+
 
         $templates = [
             'doc1' => '/public/templates/d1.docx',
             'doc2' => '/public/templates/d2.docx',
             'doc3' => '/public/templates/d3.docx',
             'doc4' => '/public/templates/d4.docx',
+            'doc5' => '/public/templates/d5.docx',
+            'doc6' => '/public/templates/d6.docx',
         ];
        
         
@@ -57,7 +94,7 @@ class WordFileController extends AbstractController
         if (!file_exists($templatePath)) {
             return new Response("Ошибка: шаблон не найден.");
         }
-        if ($documentType) {
+        if (!$documentType) {
             return new Response($documentType);
         }
        
@@ -72,6 +109,8 @@ class WordFileController extends AbstractController
         $templateProcessor->setValue('{{passportIssuedBy}}', $passportIssuedBy);
         $templateProcessor->setValue('{{passportDate}}', $passportDate);
         $templateProcessor->setValue('{{address}}', $address);
+        $templateProcessor->setValue('{{phone}}', $phone);
+        $templateProcessor->setValue('{{email}}', $email);
 
         $templateProcessor->setValue('{{name2}}', $name2);
         $templateProcessor->setValue('{{secondName2}}', $secondName2);
@@ -82,6 +121,33 @@ class WordFileController extends AbstractController
         $templateProcessor->setValue('{{passportIssuedBy2}}', $passportIssuedBy2);
         $templateProcessor->setValue('{{passportDate2}}', $passportDate2);
         $templateProcessor->setValue('{{address2}}', $address2);
+        $templateProcessor->setValue('{{phone2}}', $phone2);
+        $templateProcessor->setValue('{{email2}}', $email2);
+
+        $templateProcessor->setValue('{{name3}}', $name3); //без опекуна
+$templateProcessor->setValue('{{secondName3}}', $secondName3);
+$templateProcessor->setValue('{{thirdName3}}', $thirdName3);
+$templateProcessor->setValue('{{numberPassport3}}', $numberPassport3);
+$templateProcessor->setValue('{{serialPassport3}}', $serialPassport3);
+$templateProcessor->setValue('{{passportIssuedBy3}}', $passportIssuedBy3);
+$templateProcessor->setValue('{{passportDate3}}', $passportDate3);
+$templateProcessor->setValue('{{address3}}', $address3);
+$templateProcessor->setValue('{{phone3}}', $phone3);
+$templateProcessor->setValue('{{email3}}', $email3);
+
+        $templateProcessor->setValue('{{name4}}', $name4); //доп несоврешеннолетний свидетельство о рождении
+$templateProcessor->setValue('{{secondName4}}', $secondName4);
+$templateProcessor->setValue('{{thirdName4}}', $thirdName4);
+$templateProcessor->setValue('{{name5}}', $name5);
+$templateProcessor->setValue('{{secondName5}}', $secondName5);
+$templateProcessor->setValue('{{thirdName5}}', $thirdName5);
+$templateProcessor->setValue('{{bday}}', $bday);
+$templateProcessor->setValue('{{blocation}}', $blocation);
+$templateProcessor->setValue('{{serialSvid}}', $serialSvid);
+$templateProcessor->setValue('{{numSvid}}', $numSvid);
+$templateProcessor->setValue('{{svidIssuedBy}}', $svidIssuedBy);
+$templateProcessor->setValue('{{svidDate}}', $svidDate);
+$templateProcessor->setValue('{{address4}}', $address4);
 
 
      
